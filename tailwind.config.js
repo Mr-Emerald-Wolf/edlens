d93d6fe7cf3bee1695c9aab5e4054b9f9fcf0fa5
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-09-30 22:24:43 +0530

    EdLens front-end repo created

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+      fontFamily: {
+        'kalam': ['"Kalam"', 'cursive'],
+        'Rosarivo': ['"Rosarivo"', 'serif'],
+        'Kanit': ['"Kanit"', 'sans-serif'],
+      },
+      colors: {
+        'pale': '#edf5e1',
+        'blueTheme':"#05386B",
+        'mint':"#5CDB95"
+      },
+      backgroundImage: {
+        'hero': "url('./assets/cogs.svg')",
+      },
+		},
+	},
+}
