4a4b76a20bb66c188e69983504ededab296908f2
siddharth1825 <siddharth.dave2012@gmail.com>
2022-10-02 08:38:39 +0530

    bruh

--- src/components/Ocr.js
+++ src/components/Ocr.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createWorker } from 'tesseract.js';
+import * as THREE from '../libs/three.js-r132/build/three.module.js'
+import { GLTFLoader } from '../libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js'
+// import { ARButton } from './libs/three.js-r132/examples/jsm/webxr/ARButton.js'
 
 import '../index.css';
 
 function Ocr(props) {
+
     const worker = createWorker({
         logger: m => console.log(m),
     });
@@ -19,12 +23,122 @@ function Ocr(props) {
         console.log(text);
         return text;
     };
-    
+
     doOCR();
+
+    useEffect(()=>{
+        const script = document.createElement('script');
+
+        script.src = "../libs/mindar/mindar-image-three.prod.js"
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return()=>{
+            document.body.removeChild(script);
+        }
+    },["../libs/mindar/mindar-image-three.prod.js"])
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const initialize = async () => {
+            const arButton = document.querySelector("#ar-button");
+
+            // check and request webxr session 
+            const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
+            if (!supported) {
+                arButton.textContent = "Not Supported";
+                arButton.disabled = true;
+                return;
+            }
+
+            // build three.js scene
+            const scene = new THREE.Scene();
+            const camera = new THREE.PerspectiveCamera();
+
+            const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            renderer.setPixelRatio(window.devicePixelRatio);
+            document.body.appendChild(renderer.domElement);
+
+            const loader = new GLTFLoader();
+
+            let ocrWord = text;
+            switch (ocrWord) {
+                case 'duck':
+                    loader.load('./assets/models/Duck.gltf', function (gltf) {
+                        let duck = gltf.scene.children[0];
+                        duck.scale.set(0.01, 0.01, 0.01);
+                        duck.position.set(0, -5, -5);
+                        scene.add(gltf.scene);
+                    });
+                    break;
+                case 'fox':
+                    loader.load('./assets/models/Fox.gltf', function (gltf) {
+                        let fox = gltf.scene.children[0];
+                        fox.scale.set(0.01, 0.01, 0.01);
+                        fox.position.set(0, -5, -5);
+                        scene.add(gltf.scene);
+                    });
+                    break;
+            }
+
+            const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+            scene.add(light);
+
+            renderer.xr.addEventListener("sessionstart", (e) => {
+                console.log("session start");
+            });
+
+            renderer.xr.addEventListener("sessionend", () => {
+                console.log("session end");
+            });
+
+            let currentSession = null;
+            const start = async () => {
+                currentSession = await navigator.xr.requestSession('immersive-ar', {
+                    optionalFeatures: ['dom-overlay'],
+                    domOverlay: { root: document.body }
+                });
+
+                renderer.xr.enabled = true;
+
+
+                renderer.xr.setReferenceSpaceType('local');
+
+
+                await renderer.xr.setSession(currentSession);
+                arButton.textContent = "End";
+
+                renderer.setAnimationLoop(() => {
+                    renderer.render(scene, camera);
+
+                });
+
+            }
+            const end = async () => {
+                currentSession.end();
+                renderer.setAnimationLoop(null);
+                renderer.clear();
+                arButton.style.display = "none";
+            }
+            arButton.addEventListener('click', () => {
+                if (currentSession) {
+                    end();
+                } else {
+                    start();
+                }
+            });
+        }
+        initialize();
+
+    });
+
+    
 
 
     return (
         <>
+            <button id="ar-button">Start</button>
         </>
     );
 }
