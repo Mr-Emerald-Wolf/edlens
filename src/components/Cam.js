b40ef781544a80c2d1b3b69e685a8e41b190f8a0
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-10-02 11:47:36 +0530

    cam upgraded

--- src/components/Cam.js
+++ src/components/Cam.js
@@ -21,13 +21,14 @@ export default function Cam() {
     return (
         <>
             {!captured &&
-                <div>
+                <div className="min-h-screen bg-slate-700 grid grid-cols-1 content-center">
                     <Webcam
                         audio={false}
                         ref={webcamRef}
                         screenshotFormat="image/jpeg"
+                        className="mx-auto rounded-lg w-3/4 md:w-1/2"
                     />
-                    <button onClick={capture}>Capture photo</button>
+                    <button className="p-2 w-1/4 mx-auto text-lg text-slate-700 rounded bg-red-400 m-2" onClick={capture}>Capture photo</button>
                 </div>
             }
             {captured && <Ocr imgSrc={imgSrc}/>}
