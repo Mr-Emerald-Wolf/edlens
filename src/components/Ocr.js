ceb698c1f4093ed710b1d6a0051114348c5b3645
siddharth1825 <siddharth.dave2012@gmail.com>
2022-10-02 11:49:03 +0530

    yes

--- src/components/Ocr.js
+++ src/components/Ocr.js
@@ -24,102 +24,106 @@ function Ocr(props) {
         console.log(text);
         return text;
     };
+    
+    let word = doOCR();
 
-    doOCR();
+    const initialize = async () => {
+        const arButton = document.querySelector("#ar-button");
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const initialize = async () => {
-            const arButton = document.querySelector("#ar-button");
-
-            // check and request webxr session 
-            const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
-            if (!supported) {
-                arButton.textContent = "Not Supported";
-                arButton.disabled = true;
-                return;
-            }
-
-            // build three.js scene
-            const scene = new THREE.Scene();
-            const camera = new THREE.PerspectiveCamera();
-
-            const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            renderer.setPixelRatio(window.devicePixelRatio);
-            document.body.appendChild(renderer.domElement);
-
-            const loader = new GLTFLoader();
-
-            let ocrWord = text;
-            switch (ocrWord) {
-                case 'duck':
-                    loader.load('./assets/models/Duck.gltf', function (gltf) {
-                        let duck = gltf.scene.children[0];
-                        duck.scale.set(0.01, 0.01, 0.01);
-                        duck.position.set(0, -5, -5);
-                        scene.add(gltf.scene);
-                    });
-                    break;
-                case 'fox':
-                    loader.load('./assets/models/Fox.gltf', function (gltf) {
-                        let fox = gltf.scene.children[0];
-                        fox.scale.set(0.01, 0.01, 0.01);
-                        fox.position.set(0, -5, -5);
-                        scene.add(gltf.scene);
-                    });
-                    break;
-            }
-
-            const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-            scene.add(light);
-
-            renderer.xr.addEventListener("sessionstart", (e) => {
-                console.log("session start");
-            });
-
-            renderer.xr.addEventListener("sessionend", () => {
-                console.log("session end");
-            });
-
-            let currentSession = null;
-            const start = async () => {
-                currentSession = await navigator.xr.requestSession('immersive-ar', {
-                    optionalFeatures: ['dom-overlay'],
-                    domOverlay: { root: document.body }
-                });
-
-                renderer.xr.enabled = true;
-
-
-                renderer.xr.setReferenceSpaceType('local');
-
-
-                await renderer.xr.setSession(currentSession);
-                arButton.textContent = "End";
-
-                renderer.setAnimationLoop(() => {
-                    renderer.render(scene, camera);
-
-                });
-
-            }
-            const end = async () => {
-                currentSession.end();
-                renderer.setAnimationLoop(null);
-                renderer.clear();
-                arButton.style.display = "none";
-            }
-            arButton.addEventListener('click', () => {
-                if (currentSession) {
-                    end();
-                } else {
-                    start();
-                }
-            });
+        // check and request webxr session 
+        const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
+        if (!supported) {
+            arButton.textContent = "Not Supported";
+            arButton.disabled = true;
+            return;
         }
-        initialize();
 
-    });
+        // build three.js scene
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera();
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(renderer.domElement);
+
+        const loader = new GLTFLoader();
+
+        let ocrResult = "Duck"
+
+        switch (ocrResult) {
+            case 'DUCK ':
+            case 'Duck':
+            case 'duck':
+                loader.load('./assets/models/Duck.gltf', function (gltf) {
+                    let duck = gltf.scene.children[0];
+                    duck.scale.set(0.01, 0.01, 0.01);
+                    duck.position.set(0, -5, -5);
+                    scene.add(gltf.scene);
+                });
+                break;
+            case 'BEAR':
+            case 'Bear':
+            case 'bear':
+                loader.load('./assets/models/musicband-bear/scene.gltf', function (gltf) {
+                    let bear = gltf.scene.children[0];
+                    bear.scale.set(0.3, 0.3, 0.3);
+                    bear.position.set(0, -5, -5);
+                    scene.add(gltf.scene);
+                });
+                break;
+        }
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
+
+        renderer.xr.addEventListener("sessionstart", (e) => {
+            console.log("session start");
+        });
+
+        renderer.xr.addEventListener("sessionend", () => {
+            console.log("session end");
+        });
+
+        let currentSession = null;
+        const start = async () => {
+            currentSession = await navigator.xr.requestSession('immersive-ar', {
+                optionalFeatures: ['dom-overlay'],
+                domOverlay: { root: document.body }
+            });
+
+            renderer.xr.enabled = true;
+
+
+            renderer.xr.setReferenceSpaceType('local');
+
+
+            await renderer.xr.setSession(currentSession);
+            arButton.textContent = "End";
+
+            renderer.setAnimationLoop(() => {
+                renderer.render(scene, camera);
+
+            });
+
+        }
+        const end = async () => {
+            currentSession.end();
+            renderer.setAnimationLoop(null);
+            renderer.clear();
+            arButton.style.display = "none";
+        }
+        arButton.addEventListener('click', () => {
+            if (currentSession) {
+                end();
+            } else {
+                start();
+            }
+        });
+    }
+    initialize(word);
+
+
 
 
 
@@ -128,8 +132,10 @@ function Ocr(props) {
         <>
             <Helmet>
                 <script src="../libs/mindar/mindar-image-three.prod.js"></script>
+                <script data-consolejs-channel="b404953f-42f3-47f6-c81c-35138bd834f0" src="https://remotejs.com/agent/agent.js"></script>
+                <script src="./main.js" type="module"></script>
             </Helmet>
-                <button id="ar-button">Start</button>
+            <button id="ar-button" onClick={initialize}>Start</button>
         </>
     );
 }
